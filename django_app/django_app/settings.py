a3c26bd5f1d00e43a32c3b783290a371813ee22c
Yogesh B <ybodke123@gmail.com>
2026-01-02 15:26:43 +0530

    added FastAPI and  redis cache functionality

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -141,3 +141,15 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 
 CELERY_RESULT_EXPIRES = 60 * 60 * 24  # 1 day
+
+
+#for temporary storage of task ids.
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",  
+        "LOCATION": "redis://127.0.0.1:6379/1",      # Redis host, port, DB
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
